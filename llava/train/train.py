2b3d67235335f0e16babcb18ab5d4dd4d6d3079a
baohaoliao <liaobaohao@gmail.com>
2024-07-17 19:19:24 +0200

    debug

--- llava/train/train.py
+++ llava/train/train.py
@@ -481,14 +481,14 @@ def preprocess_lilium_2(
 
             if has_image:
                 round_len = len(tokenizer_image_token(rou, tokenizer))
-                instruction_len = len(tokenizer_image_token(parts[0], tokenizer)) - 1
+                instruction_len = len(tokenizer_image_token(parts[0], tokenizer)) - 2
             else:
                 round_len = len(tokenizer(rou).input_ids)
-                instruction_len = len(tokenizer(parts[0]).input_ids) - 1
+                instruction_len = len(tokenizer(parts[0]).input_ids) - 2
 
             target[cur_len : cur_len + instruction_len] = IGNORE_INDEX
 
-            cur_len += round_len
+            cur_len += (round_len + 1)
 
             print(f"-------{i}----------")
             print(rou)
